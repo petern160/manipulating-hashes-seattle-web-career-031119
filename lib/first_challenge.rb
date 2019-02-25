40720de5d4d22a5dca123f4742dab19072a244b5
Peter Nguyen <peternguyen160@gmail.com>
2019-02-25 01:11:38 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -2,7 +2,7 @@ def first_challenge
   contacts = {
     "Jon Snow" => {
       name: "Jon",
-      email: "jon_snow@thewall.we", 
+      email: "jon_snow@thewall.we",
       favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
       knows: nil
     },
@@ -18,11 +18,10 @@ def first_challenge
     value.each do |attribute, value|
       if attribute == :favorite_icecream_flavors
         value.shift
-      end 
+      end
     end
   end
 
   #remember to return your newly altered contacts hash!
   contacts
 end
-
